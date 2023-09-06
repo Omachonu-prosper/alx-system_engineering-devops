7893e8e91ce87060f506abd6cd49a3ff03d0b56d
Omachonu-prosper <69792620+Omachonu-prosper@users.noreply.github.com>
2023-09-06 20:07:21 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,9 @@
 # Puppet script to add a custom header to nginx and configure a server as needed
-# Install Nginx package
+# Install Nginx package and configure a custom header
 exec {'custom-header'
-  command => 'sudo apt-get update
-              sudo apt-get -y install nginx
-              # sudo chown -R $USER /etc/nginx/sites-available/
-              sudo echo "server {
-                listen 80 default_server;
-                listen [::]:80 default_server;
-                server_name _;
-                add_header X-Served-By \$hostname;
-                root /var/www/html;
-                index index.html index.htm index.nginx-debian.html;
-              }" > /etc/nginx/sites-available/default
-              sudo service nginx restart',
+  command => 'sudo apt-get update -y;
+	  sudo apt-get install nginx -y;
+	  sudo sed -i "/server_name _/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default
+	  sudo service nginx restart',
   provider => shell
 }
